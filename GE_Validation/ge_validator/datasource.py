6c9777a01287528a7557afe78f541c50e2f532bf
Karthick Dhamotharan <kdhamotharan@freewheel.tv>
2026-07-20 20:20:52 +0530

    Initial SetUP06

--- GE_Validation/ge_validator/datasource.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import great_expectations as gx
-from great_expectations.data_context import AbstractDataContext
-
-
-def get_context() -> AbstractDataContext:
-    return gx.get_context(mode="ephemeral")
